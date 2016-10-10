96a2f098348daf169f6b26fb8ebd2ed33c66ccdf
Miguel Ceballos <ceballosmiguel777@gmail.com>
2016-10-10 10:18:17 -0700

    Added more shapes

--- drawingproject.js
+++ drawingproject.js
@@ -3,11 +3,11 @@ var canvas = canvasDiv.getContext('2d');
 
 
 //background
-canvas.fillStyle = "black";
+canvas.fillStyle = "LightSkyBlue";
 canvas.fillRect(80, 150, 1000, 1000);
 
 //pink square
-canvas.fillStyle = "pink";
+canvas.fillStyle = "black";
 canvas.fillRect(80, 150, 100, 100);
 
 //blue circle
@@ -18,7 +18,7 @@ canvas.fill();
 
 //Orange triangle
 canvas.beginPath();
-canvas.fillStyle = "orange";
+canvas.fillStyle = "grey";
 canvas.moveTo(1,800);
 canvas.lineTo(1,80);
 canvas.lineTo(800,800);
@@ -26,11 +26,27 @@ canvas.fill();
 
 //Pank triangle
 canvas.beginPath();
-canvas.fillStyle = "pink";
+canvas.fillStyle = "black";
 canvas.moveTo(200,800);
 canvas.lineTo(1,80);
 canvas.lineTo(800,800);
 canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "grey";
+canvas.moveTo(400,800);
+canvas.lineTo(1,80);
+canvas.lineTo(800,800);
+canvas.fill();
+
+
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(600,800);
+canvas.lineTo(1,80);
+canvas.lineTo(800,800);
+canvas.fill();
+
 
 //
 canvas.beginPath();
@@ -42,4 +58,9 @@ canvas.fill();
 canvas.beginPath();
 canvas.fillStyle = "blue";
 canvas.arc(400,200,50,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(600,200,50,0,2*Math.PI,true);
 canvas.fill();
